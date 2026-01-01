b8ee5364e7fd0f1df88aa4ae36154c3b5dbe0253
Josh Correia <joshmcorreia@gmail.com>
2025-12-31 21:57:39 -0800

    Improve timezone usage

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,4 @@
 use chrono::{DateTime, TimeDelta, Utc};
-use chrono_tz::Tz;
 use chrono_tz::US::Pacific;
 use serde::Deserialize;
 use serenity::builder::ExecuteWebhook;
@@ -20,29 +19,25 @@ struct Config {
 }
 
 fn format_timedelta_hhmmss(delta: TimeDelta) -> String {
-    let total_seconds = delta.num_seconds();
-    let mut formatted_string = String::new();
+    let total_seconds = delta.num_seconds().max(0);
+
     let hours = total_seconds / 3600;
     let minutes = (total_seconds % 3600) / 60;
     let seconds = total_seconds % 60;
+
+    let mut parts = Vec::new();
+
     if hours > 0 {
-        let hours_string = format!("{} hours ", hours);
-        formatted_string += &hours_string;
+        parts.push(format!("{} hours", hours));
     }
     if minutes > 0 {
-        let minutes_string = format!("{} minutes ", minutes);
-        formatted_string += &minutes_string;
+        parts.push(format!("{} minutes", minutes));
     }
-    if seconds > 0 {
-        let seconds_string = format!("{} seconds ", seconds);
-        formatted_string += &seconds_string;
+    if seconds > 0 || parts.is_empty() {
+        parts.push(format!("{} seconds", seconds));
     }
 
-    if !formatted_string.is_empty() {
-        // remove the trailing space
-        formatted_string.pop();
-    }
-    return formatted_string;
+    parts.join(" ")
 }
 
 async fn internet_is_up() -> Result<bool, std::io::Error> {
@@ -75,36 +70,36 @@ async fn send_discord_message(
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let script_start_time = Utc::now().with_timezone(&Pacific);
+    let script_start_time = Utc::now();
     println!(
         "Internet Outage Duration Discord Notifier 0.1.0 initialized on {}.",
-        script_start_time.format("%m/%d/%Y %r")
+        script_start_time
+            .with_timezone(&Pacific)
+            .format("%m/%d/%Y %r")
     );
 
     let toml_content = fs::read_to_string("config.toml")?;
     let config: Config = toml::from_str(&toml_content)?;
 
-    let mut internet_outage_start_time: Option<DateTime<Tz>> = None;
-
+    let mut internet_outage_start_time: Option<DateTime<Utc>> = None;
     let http = Http::new("");
 
     loop {
         if !internet_is_up().await? {
             if internet_outage_start_time.is_none() {
-                internet_outage_start_time = Some(Utc::now().with_timezone(&Pacific));
+                let start_utc = Utc::now();
+                internet_outage_start_time = Some(start_utc);
                 println!(
                     "The internet went down at {}!",
-                    internet_outage_start_time.unwrap().format("%m/%d/%Y %r")
+                    start_utc.with_timezone(&Pacific).format("%m/%d/%Y %r")
                 );
             }
-        }
-        else if internet_outage_start_time.is_some() {
-            let outage_duration =
-                Utc::now().with_timezone(&Pacific) - internet_outage_start_time.unwrap();
+        } else if let Some(start_utc) = internet_outage_start_time {
+            let outage_duration = Utc::now() - start_utc;
             let outage_duration_hhmmss = format_timedelta_hhmmss(outage_duration);
             let internet_outage_message = format!(
                 "@everyone The internet went out at {} but is now back online. The outage lasted {}.",
-                internet_outage_start_time.unwrap().format("%m/%d/%Y %r"),
+                start_utc.with_timezone(&Pacific).format("%m/%d/%Y %r"),
                 outage_duration_hhmmss
             );
             send_discord_message(&http, &internet_outage_message, &config.webhook_url).await?;
